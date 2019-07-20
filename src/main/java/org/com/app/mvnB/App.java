2c2673e4d3487a77814554b619c8f2e807cf30d5
mohan <mohan.chanti4@gmail.com>
2019-07-20 15:37:27 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnB/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnB" );
+    }
+}
